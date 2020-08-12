3bb69b2cd9f04ef3bdbf597801461947962571b2
Tung <ntrtung17@apcs.vn>
2020-08-12 13:30:26 +0700

    Boss animation adding

---
+++ Assets/Bullet.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+
+    private Transform player;
+    private Vector2 target;
+    public GameObject hitEffect;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        target = new Vector2(player.position.x, player.position.y);
+
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime); 
+
+        if (transform.position.x == target.x && transform.position.y == target.y)
+        {
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 5f);
+            DestroyProjectile();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 1f);
+            DestroyProjectile();
+        }
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+
+    //public GameObject hitEffect;
+
+    //void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+    //    Destroy(effect, 5f);
+    //    Destroy(gameObject);
+    //}
+    
+}
---
+++ Assets/Shooting.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    public float retreaDistance;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+
+
+
+    public Transform player;
+    public GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, player.position) > stoppingDistance && Vector2.Distance(transform.position, player.position) > retreaDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, player.position) > retreaDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+
+        if (timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    //public Transform firePoint;
+    //public GameObject bulletPrefab;
+
+    //public float bulletForce = 20f;
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    // start to shoot here
+    //    if(Input.GetButtonDown("Fire1"))
+    //    {
+    //        Shoot();  
+    //    }
+    //}
+    
+    //void Shoot()
+    //{
+    //    GameObject  bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    //    Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+    //    rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse); 
+    //}
